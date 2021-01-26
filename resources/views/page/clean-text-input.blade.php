cbda8f6b4c8848dd3d272b993e8d631ab916d17f
abiyogakhusus@gmail.com <Yogaanakibusendangke2>
2021-01-26 18:46:30 +0700

    k

--- resources/views/page/clean-text-input.blade.php
+++ resources/views/page/clean-text-input.blade.php
@@ -4,13 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
     <link href="{{asset('sentiment/css/CleanText.css')}}" rel="stylesheet">
-
-
 </head>
 <body>
     <div class="row">
