c141056a4c74eebc6bbce581023d09cd524da2b7
abiyogakhusus@gmail.com <Yogaanakibusendangke2>
2021-02-24 21:09:11 +0700

    preprocessing done

--- public/sentiment/js/page/history-mining/history-mining.js
+++ public/sentiment/js/page/history-mining/history-mining.js
@@ -1,7 +1,52 @@
+
+function Deleted_UserTweet(id){
+    Swal.fire({
+        title: 'Peringatan',
+        text: "Apakah Tetap Ingin Menghapus",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Hapus'
+    }).then((confirm) => {
+         if (confirm.isConfirmed) {
+            $.ajax({
+                url : '/delete-user-mining',
+                data : {
+                    _token : dataToken,
+                    id_user : id
+                },
+                dataType : 'json',
+                type : 'post',
+                success : function(respon){
+                    if(respon.code == 200){
+                        Swal.fire({
+                            icon: 'success',
+                            title: 'Berhasil',
+                            text: respon.message,
+                        })
+                       $('.data-content').remove();
+                       HistoryMining();
+                    }
+                }
+            })   
+         }
+    })
+}
+
+function ProsesToExportDataTwitter(id){
+    // console.log(id);
+    window.location.href = "/process-export-data-mining/"+ id;
+    
+}
+
+
+
 function HistoryMining(){
   if ($(".data-content").length){
       $(".data-content").remove();
   }
+  
     $.ajax({
         url : '/index-history-mining',
         data : {},
@@ -52,7 +97,7 @@ function HistoryMining(){
                     {"targets": 5,
                       "data": 0,
                       "render": function ( data, type, row, meta ) {
-                        return ' <div class="row justify-content-center"> <div class="col" style="text-align:center"> <a href="javascript:void(0)" style="color:black" onclick="DetailProgressRitzuka('+data+')"><i class="fa fa-eye"></i></a> </div></div>';
+                        return ' <div class="row justify-content-center"> <div class="col" style="text-align:center"> <a href="javascript:void(0)" style="color:black" onclick="DetailProgressRitzuka('+data+')"><i class="fa fa-eye"></i></a> | <a href="javascript:void(0)" style="color:black" onclick="Deleted_UserTweet('+data+')"><i class="fas fa-trash-alt"></i> | <a href="javascript:void(0)" style="color:black" onclick="ProsesToExportDataTwitter('+data+')"><span class="material-icons">download_for_offline</span></a></div></div>';
                       }
                     },
                     
